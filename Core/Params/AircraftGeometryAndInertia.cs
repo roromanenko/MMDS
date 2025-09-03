bdee9cc9c4eeec18b5094fb32d64b1330c10a6ef
Roman Romanenko <roman.roromanenko@proton.me>
2025-09-03 22:16:55 +0300

    initial commit

---
+++ Core/Params/AircraftGeometryAndInertia.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Params
+{
+	public sealed class AircraftGeometryAndInertia
+	{
+		//Geometry
+		public double WingAreaSqM { get; init; } = 97.2;								//S, m^2
+		public double MeanAerodynamicChordM { get; init; } = 5.285;						// MAC, m
+
+		// Mass (kg)
+		public double FlightMassBeforeDropKg { get; init; } = 73000;					//G1
+		public double FlightMassAfterDropKg { get; init; } = 68000;						//G2
+
+		// Center of Gravity (% of MAC)
+		public double CgBeforeDropPercentMac { get; init; } = 24;						// x̄_t1
+		public double CgAtReleasePercentMac { get; init; } = 30;						// x̄_c
+		public double CgAfterDropPercentMac { get; init; } = 24;						// x̄_t2
+
+		// Longitudinal moment of inertia (kg·m^2).
+		public double LongitudinalInertiaBeforeDropKgM2 { get; init; } = 660000;		// I_z1
+		public double LongitudinalInertiaAfterDropKgM2 { get; init; } = 650000;			// I_z2
+	}
+}
---
+++ Core/Params/FlightAndAeroParams.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Params
+{
+	public sealed class FlightAndAeroParams
+	{
+		// Flight conditions
+		public double VelocityMS { get; init; } = 97.2;					// V0, m/s
+		public double AltitudeM { get; init; } = 600;					// H0, m
+		public double AirDensityKgS2M4 { get; init; } = 0.119;			// ρ, кг·с^2/м^4
+		public double GravityMS2 { get; init; } = 9.81;					// g, m/s^2
+		public double SpeedOfSoundMS { get; init; } = 338.36;			// aH, m/s
+
+		// Aerodynamic coefficients
+		public double Cy0 { get; init; } = -0.255;
+		public double CyAlpha { get; init; } = 5.78;
+		public double CyDeltaV { get; init; } = 0.2865;
+		public double Cx { get; init; } = 0.13;
+
+		// Pitching moment derivatives
+		public double Mz0 { get; init; } = 0.2;
+		public double MzOmegaZ { get; init; } = -13;
+		public double MzAlphaDot { get; init; } = -3.8;
+		public double MzAlpha { get; init; } = -1.83;
+		public double MzDeltaV { get; init; } = -0.96;
+	}
+}
